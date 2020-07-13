41d2da5c717e38823c7eeac12529cf28bcb32813
HaiNguyen3112 <nguyenhai311294@gmail.com>
2020-07-13 16:40:28 +0700

    First SQL

---
+++ thuc_thi_cau_lenh_SQL.sql
@@ -0,0 +1,6 @@
+SELECT * FROM classicmodels.customers;
+select * from customers;
+update customers set customerName = "Hai" where city = "Manchester";
+SELECT customerName, phone, city, country FROM customers ;
+SELECT * FROM customers WHERE customername like '%A%';
+SELECT * FROM customers WHERE city IN ('Nantes',' Las Vegas',' Warszawa','NYC'); 
